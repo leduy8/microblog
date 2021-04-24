57af93b89c86a1f7fe18329daa4e3041b2b1e36d
duy.leduc8 <duy.leduc8@gmail.com>
2021-04-24 13:18:18 +0700

    fix can't find installed packages error

--- tests.py
+++ tests.py
@@ -22,7 +22,9 @@ class UserModelCase(unittest.TestCase):
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128), ('https://gravatar.com/avatar/''d4c74594d841139328695756648b6bd6''?d=identicon&s=128'))
+        self.assertEqual(u.avatar(128), ('https://gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
